2e1e60d0dfaeec5b0390b3c001373d7ae2be9f90
dpaschal-lf <daniel.paschal@gmail.com>
2019-11-25 23:42:26 -0800

    added basic operation script for sand box

--- testingsandbox/main.js
+++ testingsandbox/main.js
@@ -1,13 +1,15 @@
 $(document).ready(function(){
 console.log( testCode( `
 /* prep code */
-var t1 = 'daniel';
-var t2 = 'david';
+
+var testMe = true;
+var result = null;
+
+if( testMe ){
+    result = true;
+}
 /*end prep code */
 /* student code */
-
-var answer = t1 > t2;//false
-
 
 
 /* end student code */
